41089fa39c9f7be43ffec6b444c1fb207b342a60
Geovany <egoo.geovany@gmail.com>
2016-09-23 19:24:10 -0600

    Segundo Comit, listado de articulos publicados

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from blog.models import Articulo
 
 # Create your views here.
 def articulo_lista(request):
-    return render(request, 'blog/articulo_lista.html', {})
+    articulos = Articulo.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'blog/articulo_lista.html', {'articulos':articulos})
