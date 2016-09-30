de357ac24ae8686a0fdf9f073350b26652c2b3bf
Geovany <egoo.geovany@gmail.com>
2016-09-29 23:34:07 -0600

    Cambios..CSS y extend

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from blog.models import Articulo
 
@@ -6,3 +6,8 @@ from blog.models import Articulo
 def articulo_lista(request):
     articulos = Articulo.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/articulo_lista.html', {'articulos':articulos})
+
+def articulo_detalle(request, pk):
+        post = get_object_or_404(Articulo, pk=pk)
+        return render(request, 'blog/articulo_detalle.html', {'post': post})
+        
