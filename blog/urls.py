ba1080fc962ba92bd401b408af90734369e22fe6
Geovany <egoo.geovany@gmail.com>
2016-10-11 21:10:37 -0600

    Crear nuevo y editar

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.articulo_lista),
     url(r'^post/(?P<pk>[0-9]+)/$', views.articulo_detalle),
-    
+    url(r'^post/new/$', views.articulo_nuevo, name='articulo_nuevo'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.articulo_editar, name='articulo_editar'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from blog.models import Articulo
+from .forms import ArticuloForm
+
 
 # Create your views here.
 def articulo_lista(request):
@@ -10,4 +12,29 @@ def articulo_lista(request):
 def articulo_detalle(request, pk):
         post = get_object_or_404(Articulo, pk=pk)
         return render(request, 'blog/articulo_detalle.html', {'post': post})
-        
+
+def articulo_nuevo(request):
+    if request.method == "POST":
+        formulario = ArticuloForm(request.POST)
+        if formulario.is_valid():
+            articulo = formulario.save(commit=False)
+            articulo.autor = request.user
+            articulo.fecha_publicacion = timezone.now()
+            post.save()
+            return redirect('blog.views.articulo_detalle', pk=articulo.pk)
+    else:
+        formulario = ArticuloForm()
+    return render(request, 'blog/articulo_editar.html', {'formulario': formulario})
+
+def articulo_editar(request, pk):
+    articulo = get_object_or_404(Articulo, pk=pk)
+    if request.method == "POST":
+        formulario = ArticuloForm(request.POST, instance=articulo)
+        if formulario.is_valid():
+            articulo = formulario.save(commit=False)
+            articulo.autor = request.user
+            articulo.save()
+            return redirect('blog.views.articulo_detalle', pk=articulo.pk)
+    else:
+        formulario = ArticuloForm(instance=articulo)
+    return render(request, 'blog/articulo_editar.html', {'formulario': formulario})
